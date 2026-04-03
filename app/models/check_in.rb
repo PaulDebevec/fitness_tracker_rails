3ffd362fff438f72159bc8b7705325f9ee6cdbfa
Paul Debevec <paulmdebevec@gmail.com>
2026-04-03 10:48:25 -0600

    Update checkins model and relationship with profile

--- app/models/check_in.rb
+++ app/models/check_in.rb
@@ -1,3 +1,13 @@
 class CheckIn < ApplicationRecord
-    belongs_to :profile # Add model test for CheckIn
+    belongs_to :profile
+
+    validates :checked_in_on, presence: true
+    validate :checked_in_on_cannot_be_in_the_future
+
+    private
+    def checked_in_on_cannot_be_in_the_future
+        return if checked_in_on.blank?
+        return unless checked_in_on > Date.current
+        errors.add(:checked_in_on, "can't be in the future")
+    end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,16 +1,17 @@
 class Profile < ApplicationRecord
-    has_many :check_ins, dependent: :destroy # Update model test to cover this
-    validates :display_name, presence: true, length: { minimum: 2, maximum: 50 }
-    validates :default_unit, presence: true, inclusion: { in: %w[in cm] }
+  has_many :check_ins, dependent: :destroy
 
-    def formatted_default_unit
-        case default_unit
-        when "in"
-          "Inches"
-        when "cm"
-          "Centimeters"
-        else
-          default_unit
-        end
-      end
+  validates :display_name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :default_unit, presence: true, inclusion: { in: %w[in cm] }
+
+  def formatted_default_unit
+    case default_unit
+    when "in"
+      "Inches"
+    when "cm"
+      "Centimeters"
+    else
+      default_unit
+    end
+  end
 end
