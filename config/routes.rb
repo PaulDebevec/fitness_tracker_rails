dcbf0d06356f57d7b72f38724ff20d5f1c700bb1
Paul Debevec <paulmdebevec@gmail.com>
2026-04-21 20:41:00 -0600

    Replace profile deletion with account deletion through UsersController#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  resources :users, only: [:destroy]
   resources :profiles, except: [:new, :create] do
     resource :report, only: [:show]
     resources :check_ins do
