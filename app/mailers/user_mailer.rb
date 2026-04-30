d22fce9568a056406db1bb87b4cb8497a48d7e14
Paul Debevec <paulmdebevec@gmail.com>
2026-04-30 12:12:15 -0600

    Add user mailer for email verification and password reset

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+  def email_verification
+    @user = params[:user]
+    @verification_url = email_verification_url(token: @user.email_verification_token)
+
+    mail(to: @user.email, subject: "Verify your BodiMetrix email")
+  end
+
+  def password_reset
+    @user = params[:user]
+    @reset_url = edit_password_reset_url(token: @user.password_reset_token)
+
+    mail(to: @user.email, subject: "Reset your BodiMetrix password")
+  end
+end
